b4792c428d98b7ccadacc1d497952d5bf2291596
Rômulo Nunes <romulotg21@gmail.com>
2023-10-16 15:12:13 -0300

    feat: tratativas para arquivos e diretorios e passagem de argumentos

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,29 @@
+import fs from "fs";
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
 import { trataErro } from "./index.js";
 
-const caminho = process.argv;
+const caminho = process.argv[2];
+
+function imprimeLista(resultado) {
+  console.log(chalk.yellow("Lista de links:"), resultado);
+}
 
 async function processaTexto(caminho) {
   try {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow("Lista de Links: "), resultado);
+    //valida se é arquivo
+    if (fs.lstatSync(caminho).isFile()) {
+      const resultado = await pegaArquivo(caminho);
+      imprimeLista(resultado);
+      //valida se é um diretorio e retorna a leitura do que há dentro
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+      const arquivos = await fs.promises.readdir(caminho);
+      arquivos.forEach(async (nomeDosArquivos) => {
+        const lista = await pegaArquivo(`${caminho}/${nomeDosArquivos}`);
+        imprimeLista(lista);
+      });
+      console.log(arquivos);
+    }
   } catch (err) {
     await trataErro(err);
   }
