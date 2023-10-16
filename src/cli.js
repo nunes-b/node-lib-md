2275e99bfd5d460ec09118cd5a6e731b44959736
Rômulo Nunes <romulotg21@gmail.com>
2023-10-16 16:09:08 -0300

    fix: adicionando async em functions

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,26 @@
 import fs from "fs";
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
+import listaValidada from "./http-validacao.js";
 import { trataErro } from "./index.js";
 
 const caminho = process.argv[2];
+const valida = process.argv[3] === "--valida"; // true or false
 
-function imprimeLista(resultado, identificador = "") {
-  console.log(
-    chalk.yellow("Lista de links:"),
-    chalk.black.bgGreen(identificador),
-    resultado
-  );
+async function imprimeLista(valida, resultado, identificador = "") {
+  if (valida) {
+    console.log(
+      chalk.yellow("Lista validada"),
+      chalk.black.bgGreen(identificador),
+      await listaValidada(resultado)
+    );
+  } else {
+    console.log(
+      chalk.yellow("Lista de links:"),
+      chalk.black.bgGreen(identificador),
+      resultado
+    );
+  }
 }
 
 async function processaTexto(caminho) {
@@ -18,13 +28,13 @@ async function processaTexto(caminho) {
     //valida se é arquivo
     if (fs.lstatSync(caminho).isFile()) {
       const resultado = await pegaArquivo(caminho);
-      imprimeLista(resultado);
+      imprimeLista(valida, resultado);
       //valida se é um diretorio e retorna a leitura do que há dentro
     } else if (fs.lstatSync(caminho).isDirectory()) {
       const arquivos = await fs.promises.readdir(caminho);
       arquivos.forEach(async (nomeDosArquivos) => {
         const lista = await pegaArquivo(`${caminho}/${nomeDosArquivos}`);
-        imprimeLista(lista, nomeDosArquivos);
+        imprimeLista(valida, lista, nomeDosArquivos);
       });
       console.log(arquivos);
     }
